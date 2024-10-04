f0a0dfbf7bc0df6a9b6602ea3b02d4fa685425e9
DasHam0123 <flores.charlie942@gmail.com>
2024-10-03 22:14:28 -0400

    changes homepage image to a slideshow

---
+++ scripts.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    // Increment slide index
+    slideIndex++;
+    // Reset to first slide if index exceeds the number of slides
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    // Display the current slide
+    slides[slideIndex - 1].style.display = "block";
+    // Change slide every 5 seconds
+    setTimeout(showSlides, 10000);
+}
